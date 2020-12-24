ff791e584f1370e8682b87a38dee29bd6c4e6387
Setu Potnis <setupotnis@gmail.com>
2020-12-24 14:34:06 -0500

    add setup for scalable component imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-import Products from "./Components/Products/Products";
+import { Products, Navbar } from "./Components";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Products />
     </div>
   );
